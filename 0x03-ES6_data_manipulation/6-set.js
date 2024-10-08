4b44ac08587680a0af1f0b944ae1251bd01ddd99
sany <sandersonnyange@gmail.com>
2024-10-08 14:51:14 -0700

    Task 6: Creates a set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Sanderson Nyange<https://github.com/nyvnge>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
